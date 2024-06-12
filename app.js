712b6473ed3885e55a96c2021540e0e4c7173d06
abner0samuel <abaretabner@gmail.com>
2024-06-12 09:49:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const adID = document.querySelector(".container > .advice > .name");
+const advice= document.querySelector("container .text p");
+const btn = document.querySelector(".container>.svg2x")
+const id =  document.querySelector(".container> .advice>.name>.id")
+
+
+function app(){
+    
+}
